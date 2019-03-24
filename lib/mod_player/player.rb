5dd20b2f83a7659b43353d7cf20a2c14c6a52896
Robert Haines <rhaines@manchester.ac.uk>
2019-03-24 18:03:55 +0000

    Add code to parse CLI parameters.
    Initial repeat option is included but not wired in yet.

--- lib/mod_player/player.rb
+++ lib/mod_player/player.rb
@@ -1,10 +1,48 @@
 # frozen_string_literal: true
 
-# Copyright (c) 2018 Robert Haines.
+# Copyright (c) 2018, 2019 Robert Haines.
 #
 # Licensed under the BSD License. See LICENCE for details.
+
+require 'optparse'
 
 require 'mod_player/version'
 require 'mod_player/audio'
 require 'mod_player/audio/portaudio'
 require 'mod_player/ui'
+
+module ModPlayer
+  class Player
+
+    def self.read_opts(args)
+      options = {
+        repeat: 0
+      }
+
+      opts = OptionParser.new do |opt|
+        opt.banner = 'Usage: player [options] MOD'
+        opt.separator '  Play a mod/tracker file. [options] can be:'
+        opt.on('-r[N]', '--repeat[=N]', 'Repeat the mod N times. ' \
+          'Omit N to repeat forever. Default is no repeat.') do |val|
+          options[:repeat] = val.nil? ? -1 : val.chomp.to_i
+        end
+
+        opt.on_tail('-h', '-?', '--help', 'Show this help message.') do
+          puts opts
+          exit
+        end
+        opt.on_tail('-v', '--version', 'Show the version.') do
+          puts ModPlayer::VERSION
+          exit
+        end
+      end
+
+      opts.parse! args
+
+      options[:file] = args[0].chomp
+
+      options
+    end
+
+  end
+end
