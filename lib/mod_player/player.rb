f2f6ec538ba8ac4ae5689983c61073b4577ee0d8
Robert Haines <rhaines@manchester.ac.uk>
2019-03-25 19:10:24 +0000

    Refactor to move player functionality into the library.

--- lib/mod_player/player.rb
+++ lib/mod_player/player.rb
@@ -6,6 +6,8 @@
 
 require 'optparse'
 
+require 'ffi/openmpt'
+
 require 'mod_player/version'
 require 'mod_player/audio'
 require 'mod_player/audio/portaudio'
@@ -13,6 +15,40 @@ require 'mod_player/ui'
 
 module ModPlayer
   class Player
+
+    def initialize(options)
+      init_mod(options)
+      init_audio
+      init_ui(options[:file])
+    end
+
+    def stop
+      @buffer.free
+      @audio.stop
+      @ui.close
+      @mod.close
+    end
+
+    def play
+      loop do
+        if @ui.paused?
+          sleep(1)
+          @ui.listen
+          next
+        end
+
+        can_write = @audio.write_available
+
+        next unless can_write.positive?
+
+        has_read = @mod.read_interleaved_float_stereo(can_write, @buffer)
+
+        break unless has_read.positive?
+
+        @audio.write(@buffer, has_read)
+        @ui.listen
+      end
+    end
 
     def self.read_opts(args)
       options = {
@@ -44,5 +80,26 @@ module ModPlayer
       options
     end
 
+    private
+
+    def init_mod(options)
+      @mod = ::FFI::OpenMPT::Module.new(options[:file])
+      @mod.repeat_count = options[:repeat]
+    end
+
+    def init_audio
+      @audio = ModPlayer::PortAudio.new
+      @audio.start
+
+      buffer_max_frames = @audio.write_available * 2
+      @buffer = ::FFI::MemoryPointer.new(:float,
+                                         buffer_max_frames * @audio.channels)
+    end
+
+    def init_ui(file)
+      @ui = ModPlayer::UI.new(@mod, file)
+      @ui.open
+      @ui.draw
+    end
   end
 end
--- lib/mod_player/ui.rb
+++ lib/mod_player/ui.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
-# Copyright (c) 2018 Robert Haines.
+# Copyright (c) 2018, 2019 Robert Haines.
 #
 # Licensed under the BSD License. See LICENCE for details.
 
 require 'curses'
+
+require 'mod_player/version'
 
 module ModPlayer
   class UI
